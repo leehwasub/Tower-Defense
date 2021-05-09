54d563ef3c3bc97522139bd0dff513c8cf053daa
leehwasub <hwasub1115@naver.com>
2021-05-09 18:09:22 +0900

    Initial commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab; //적 프리팹
+    [SerializeField]
+    private float spawnTime; //적 생성주기
+    [SerializeField]
+    private Transform[] wayPoints; //현재 스테이지의 이동 경로
+
+    private void Awake()
+    {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            GameObject clone = Instantiate(enemyPrefab); //적 오브젝트 생성
+            Enemy enemy = clone.GetComponent<Enemy>(); //방금 생성된 적의 Enemy 컴포넌트
+
+            enemy.Setup(wayPoints); //wayPoint정보를 매개변수로 Setup() 호출
+
+            yield return new WaitForSeconds(spawnTime); //spawnTime 시간 동안 대기
+        }
+    }
+}
